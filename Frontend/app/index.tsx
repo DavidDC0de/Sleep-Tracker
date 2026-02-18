a8a9d0e2378243b05c8a623153e635eaf797e968
David <davidavis2004@gmail.com>
2026-02-18 00:45:48 +0000

    update frontend folder with only necessary files

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from 'react-native';
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Hello Sleep Tracker </Text>
+        justifyContent: "center",
+        alignItems: "center",
+      }} 
+    > 
+    
+      <text>Start in here: app/index.tsx</text>
+      
     </View>
   );
 }
